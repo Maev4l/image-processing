be32f7c17d8d7effca219b525cc32f55d45fc4b7
Maev4l <maeval.nightingale@gmail.com>
2019-03-16 17:23:50 +0100

    Add capability to resize an image given its url

--- handler.js
+++ handler.js
@@ -1,13 +1,27 @@
 const sharp = require('sharp');
 const Joi = require('joi');
+const httpclient = require('request');
 
 /* eslint-disable newline-per-chained-call */
-const resizeSchema = Joi.object().keys({
-  width: Joi.number().positive().max(1024).integer().required(),
-  height: Joi.number().positive().max(1024).integer().required(),
-  image: Joi.string().base64().required(),
-  format: Joi.string().allow('png').required(),
-});
+const resizeSchema = Joi.object()
+  .keys({
+    width: Joi.number()
+      .positive()
+      .max(1024)
+      .integer()
+      .required(),
+    height: Joi.number()
+      .positive()
+      .max(1024)
+      .integer()
+      .required(),
+    image: Joi.string().base64(),
+    imageUrl: Joi.string().uri(),
+    format: Joi.string()
+      .allow('png')
+      .required(),
+  })
+  .nand(['image', 'imageUrl']);
 
 const makeResponse = (statusCode, result) => {
   let body = '';
@@ -26,6 +40,17 @@ const makeResponse = (statusCode, result) => {
   return response;
 };
 
+const transformImage = (buffer, width, height, format, callback) =>
+  sharp(buffer)
+    .resize(width, height)
+    .max()
+    .toFormat(format)
+    .toBuffer((err, data) => {
+      if (err) {
+        return callback(null, makeResponse(500, { message: err.message }));
+      }
+      return callback(null, makeResponse(200, { resized: data.toString('base64') }));
+    });
 
 const resize = (event, context, callback) => {
   const request = JSON.parse(event.body);
@@ -38,24 +63,20 @@ const resize = (event, context, callback) => {
   }
 
   const {
-    id, width, height, format, image,
+    width, height, format, image, imageUrl,
   } = request;
 
-  const buf = Buffer.from(image.trim(), 'base64');
-
-  return sharp(buf).resize(width, height)
-    .max()
-    .toFormat(format)
-    .toBuffer((err, data) => {
+  if (imageUrl) {
+    return httpclient({ uri: imageUrl, method: 'GET', encoding: null }, (err, resp, buffer) => {
       if (err) {
         return callback(null, makeResponse(500, { message: err.message }));
       }
-      return callback(null, makeResponse(200, {
-        id,
-        resized: data.toString('base64'),
-      }));
+      return transformImage(buffer, width, height, format, callback);
     });
-};
+  }
 
+  const buf = Buffer.from(image.trim(), 'base64');
+  return transformImage(buf, width, height, format, callback);
+};
 
 exports.resize = resize;
